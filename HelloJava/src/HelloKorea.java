82e9a40723271358beba31f61276a50c599d6e01
jinwoo8420 <1223wlsdn@naver.com>
2022-03-22 09:19:30 +0900

    Hello

---
+++ HelloJava/src/HelloKorea.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ */
+//소스코드는 반드시 public class 클래스명 형식으로 시작
+
+//Camel Case
+//두 개 이상의 단어를 조합하여 만드는 것
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println("우리나라만세");
+	}
+}
---
+++ HelloKorea.java
@@ -0,0 +1,5 @@
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println("HelloKorea");		
+	}
+}
