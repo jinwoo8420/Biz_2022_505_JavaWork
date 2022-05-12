c8d9ce90809e94e125a8b2a741fc328c8d1fecb7
jinwoo8420 <1223wlsdn@naver.com>
2022-05-12 09:17:11 +0900

    0512

--- Java_06_App_03_Todo/src/com/callor/todo/exec/VarEx1.java
+++ Java_06_App_03_Todo/src/com/callor/todo/exec/VarEx1.java
@@ -1,9 +1,30 @@
 package com.callor.todo.exec;
 
+import com.callor.todo.model.TodoVO;
+
 public class VarEx1 {
 
 	public static void main(String[] args) {
+		int num1;
+		long num2;
+		float num3;
 		
+		/*
+		 * 클래스 Type 변수, 참조형 변수
+		 * 
+		 * 몇가지 method가 속성들이 자동으로 생성되어 사용 할 수 있도록 미리 구성된다
+		 */
+
+		// wrapper 클래스 변수
+		Integer num4;
+		Long num5;
+		Float num6;
+
+		// TodoVO 클래스 변수
+		TodoVO toVO = new TodoVO();
+
+		// 배열
+		int[] nums = new int[10];
 	}
 
 }
